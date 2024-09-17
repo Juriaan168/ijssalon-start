d5ce8fd1663b78b3f8d05993f6dd968756ce0091
Juriaan <JuriaanKnol@gmail.com>
2024-09-17 15:33:02 +0200

    functies toegevoegd

--- tijdelijk.py
+++ tijdelijk.py
@@ -1,45 +1,42 @@
-# prijslijst
-prijzen = {
- "aardbei" : 3,
- "vanille" : 4,
- "chocolade" : 5
- }
-
-# aanbieding want vanille is goedkoper
-aanbieding = prijzen["aardbei"] * 0.8 
+from helper import decoreer
 
 
-# reclame text van aanbeiding
-reclame_tekst = "Vandaag in de aanbieding: aardbei-ijs, 1 liter - slechts €" + str(aanbieding)
+def print_aanbieding():
+    # prijslijst
+    prijzen = {
+    "aardbei" : 3,
+    "vanille" : 4,
+    "chocolade" : 5
+    }
+
+    # aanbieding want vanille is goedkoper
+    aanbieding = prijzen["aardbei"] * 0.8 
 
 
-# Afronden naar 2 tekens achter comma
-reclame_tekst2 = reclame_tekst[:62]
+    # reclame text van aanbeiding
+    reclame_tekst = "Vandaag in de aanbieding: aardbei-ijs, 1 liter - slechts €" + str(aanbieding)
 
 
-# Oom ellie met sportvliegtuig
-reclame_tekst3 = reclame_tekst2.upper()
+    # Afronden naar 2 tekens achter comma
+    reclame_tekst2 = reclame_tekst[:62]
 
 
-# List van woorden
-reclame_tekst4 = reclame_tekst3.split(" ")
+    # Oom ellie met sportvliegtuig
+    reclame_tekst3 = reclame_tekst2.upper()
 
 
-# de for loop alles onder elkaar
-for el in reclame_tekst4:
-    if len(el) < 5:
-     print(el.lower())
-    else:
-     print(el.upper()) 
-     # In princiepe is de upper in de else stakement hier overbodig omdat bij de stap van het spoortvliegtuig alles al in upper gezet was. Maar will wel netjes consistent blijven met de if stakement war lower wel genoed is.
+    # List van woorden
+    reclame_tekst4 = reclame_tekst3.split(" ")
 
 
+    # de for loop alles onder elkaar
+    for el in reclame_tekst4:
+        if len(el) < 5:
+         print(el.lower())
+        else:
+            print(el.upper()) 
+            # In princiepe is de upper in de else stakement hier overbodig omdat bij de stap van het spoortvliegtuig alles al in upper gezet was. Maar will wel netjes consistent blijven met de if stakement war lower wel genoed is.
 
 
-#Hiermee kan ik kijken of alle vraiable kloppen. Ze kloppen allemaal dus ik heb dit effe om non-actief gezet met hastag
-#print(prijzen["aardbei"])
-#print(aanbieding)
-#print(reclame_tekst)
-#print(reclame_tekst2)
-#print(reclame_tekst3)
-#print(reclame_tekst4)
+decoreer("Aanbieding")
+print_aanbieding()
