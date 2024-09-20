b5b2ca344bc68ca6fdd2a92ed3113b400992c617
Juriaan <JuriaanKnol@gmail.com>
2024-09-20 14:10:26 +0200

    Passende tekst voor les 10

--- helper.py
+++ helper.py
@@ -5,3 +5,23 @@ def decoreer(tekst=""):
     print(f"* {tekst} *")
     print(lengte * "*")
     print()
+
+def fooi_pp(bedrag, personen):         
+    bedrag_pp = bedrag/personen 
+    return f"Het bedrag per persoon is {bedrag_pp} euro"
+       
+
+def onderstreep(tekst = ""):
+    uit = []    
+    uit.append(tekst)
+    uit.append("=" * len(tekst))
+    return uit
+
+def som(input_dict):
+
+    #Pak alle values en zet ze achter elkaar als array/list, want keys hebben we niks aan bij totaal bereken
+    output_dict = []
+    for key in input_dict.keys() :
+        output_dict.append(input_dict[key])
+    return sum(output_dict)
+
